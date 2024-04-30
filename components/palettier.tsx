df48ce983804076c2b15b38c26b44893c1ea89d2
Namide <damien@doussaud.fr>
2024-04-30 15:10:20 +0200

    Add limits

--- components/palettier.tsx
+++ components/palettier.tsx
@@ -6,22 +6,22 @@ import Palettier3d from "@/components/palettier3d"
 export default function Palettier() {
   const sizes = useControls({
     Palette: folder({
-      palletLength: { value: 1200, label: "Longueur mm" },
-      palletWidth: { value: 800, label: "Largeur mm" },
-      palletHeight: { value: 100, label: "Hauteur mm" },
+      palletLength: { value: 1200, label: "Longueur mm", min: 1, max: 5000 },
+      palletWidth: { value: 800, label: "Largeur mm", min: 1, max: 5000 },
+      palletHeight: { value: 100, label: "Hauteur mm", min: 1, max: 300 },
     }),
     "Dimensions d'un lit": folder({
       "Sur la longueur de la palette": folder({
-        boxLengthCount: { value: 3, label: "Nbre" },
-        boxLengthSize: { value: 250, label: "Dimension" },
+        boxLengthCount: { value: 3, label: "Nbre", min: 1, max: 30 },
+        boxLengthSize: { value: 250, label: "Dimension", min: 1, max: 1000 },
       }),
       "Sur la largeur de la palette": folder({
-        boxWidthCount: { value: 2, label: "Nbre" },
-        boxWidthSize: { value: 300, label: "Dimension" },
+        boxWidthCount: { value: 2, label: "Nbre", min: 1, max: 30 },
+        boxWidthSize: { value: 300, label: "Dimension", min: 1, max: 1000 },
       }),
-      boxHeight: { value: 200, label: "Hauteur" },
+      boxHeight: { value: 200, label: "Hauteur", min: 1, max: 1000 },
     }),
-    boxFloorsCount: { value: 2, label: "Nbre de lits" },
+    boxFloorsCount: { value: 2, label: "Nbre de lits", min: 1, max: 100 },
   })
 
 
